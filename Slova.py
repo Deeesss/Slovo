756b618572565978044704ce020bea5622830cb8
Deeesss <111076028+Deeesss@users.noreply.github.com>
2023-09-22 22:26:55 +0200

    Add elif for more, still basic

--- Slova.py
+++ Slova.py
@@ -1,5 +1,8 @@
 slovo = input("Zadej random slovo: ")
-if len(slovo) >= 3 and len(slovo) <= 10:
-    print("Normálne slovo")
+if len(slovo) <= 3:
+    print("Kratke slovo")
+elif len(slovo) <= 20:
+    print("Dlhé slovo!")
+
 else:
-    print("Dlhé slovo alebo krátke slovo!")
+    print("Mega dlhe slovo")
